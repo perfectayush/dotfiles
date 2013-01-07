18f9d526feee480aa7fcdf5b69e12fa4cc8147ca
Ayush Goyal <ayush.go@directi.com>
2013-01-07 23:30:18 +0530

    Auto install emacs-starter-kit from ELPA

--- .emacs.d/custom/dependencies-setup.el
+++ .emacs.d/custom/dependencies-setup.el
@@ -16,6 +16,17 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
 
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-ruby starter-kit-eshell starter-kit-js)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 ;; Load el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
