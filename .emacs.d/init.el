a5dd0235abe11c527dda143e9dfaf0ccdae9b0f3
Ayush Goyal <ayush.go@directi.com>
2013-04-03 02:29:16 +0530

    remove unused config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,12 +4,3 @@
 (require 'modeline-tweaks)
 (require 'linum-relativenumber)
 (require 'default-tweaks)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("2611d548a133727b1a956c4fa262935e993662a0aaa25d9532b04ab8f556a25b" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "a81bc918eceaee124247648fc9682caddd713897d7fd1398856a5b61a592cb62" "777b246f60144b6051a87b80d62a7a55c09381321fcf8bcc5624d39bc517fa31" "1045eb8ac1266e3dd007b7a44fca836432e1b4439a3c8bccb0a7068f16970527" "211bb9b24001d066a646809727efb9c9a2665c270c753aa125bace5e899cb523" "71efabb175ea1cf5c9768f10dad62bb2606f41d110152f4ace675325d28df8bd" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default))))
