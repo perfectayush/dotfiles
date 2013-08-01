8b0ff11cac83f86f48ae590b9877eaba645eca6f
Ayush Goyal <ayush.go@directi.com>
2013-08-02 03:59:10 +0530

    color changes in linum-relativenumbers.el

--- emacs.d/custom/linum-relativenumber.el
+++ emacs.d/custom/linum-relativenumber.el
@@ -21,31 +21,32 @@
 (defadvice linum-update (before linum-relativenumber-linum-update activate)
   (setq-default linum-last-pos (line-number-at-pos)))
 
+(set-face-attribute 'linum nil :foreground "#111" :background "grey70")
+
 (defface linum-relativenumber-zero
-  '((t :inherit linum :foreground "grey10" :background "#31A39C" :weight bold))
+  '((t :inherit linum :foreground "grey70" :background "#5F00AF" :weight bold))
   "Face for displaying line number 0"
   :group 'linum)
 
 (defface linum-relativenumber-top
-  '((t :inherit linum :foreground "grey80" :weight normal))
+  '((t :inherit linum :weight normal))
   "Face for displaying top line number"
   :group 'linum)
 
 (defface linum-relativenumber-line
-  '((t :inherit linum :foreground "grey80" :weight normal))
+  '((t :inherit linum :foreground "grey70" :background "#111" :weight normal))
   "Face for displaying absolute line number"
   :group 'linum)
 
 (defun linum-relativenumber-format (line-number)
   (let ((diff (abs (- line-number linum-last-pos))))
-    (concat   (propertize (format "%4d \u2502" line-number)
+    (concat   (propertize (format "%3d " line-number)
                         'face 'linum-relativenumber-line)
 
-              (propertize (format "%3d \u2502" diff)
+              (propertize (format " %2d " diff)
                         'face (cond ((zerop diff) 'linum-relativenumber-zero)
                                     ((eq 1 line-number) 'linum-relativenumber-top)
-                                    (t 'linum)))
-                        )))
+                                    (t 'linum))))))
 
 (setq linum-format 'linum-relativenumber-format)
 
