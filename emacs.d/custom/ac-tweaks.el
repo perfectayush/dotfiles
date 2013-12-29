11d5eddd9bad37268f7082d888f50b83c350934c
Ayush Goyal <ayush.go@directi.com>
2013-12-30 04:51:50 +0530

    add some comments for differentiating sections

--- emacs.d/custom/ac-tweaks.el
+++ emacs.d/custom/ac-tweaks.el
@@ -19,10 +19,12 @@
 (define-key ac-completing-map (kbd "<backtab>") 'ac-previous)
 
 
+;; jedi
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t) 
 (setq jedi:complete-on-dot t)             
 
+;; ruby robe mode
 (add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'robe-mode-hook
           (lambda ()
