87f1a0051aaa27dbf6a9aca6c36e23dc610f8259
Ayush Goyal <ayush.go@directi.com>
2015-02-27 18:44:34 +0530

    some evil tweaks for minibuffer

--- emacs.d/custom/evil-tweaks.el
+++ emacs.d/custom/evil-tweaks.el
@@ -99,6 +99,30 @@
 (define-key evil-motion-state-map "\M-p" 'evil-paste-pop)
 (define-key evil-motion-state-map "\M-P" 'evil-paste-pop-next)
 
+;; j/k for browsing wrapped lines
+(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+
+;; esc quits
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark  t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
 
 ;; evil-org-mode
 (require 'org)
