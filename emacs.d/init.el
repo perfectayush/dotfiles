7db8b7bcb5130c24468c8c6fa14b41245c7c7bf0
Ayush Goyal <ayush.go@directi.com>
2015-07-05 22:35:32 +0530

    add some custom variable options

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,3 +8,16 @@
 (require 'mpris-defuns)
 (require 'misc-tweaks)
 (require 'linum-relativenumber)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-highlight-parentheses-mode t)
+ '(semantic-mode t)
+ '(show-paren-mode nil)
+ '(sp-highlight-pair-overlay nil)
+ '(sp-highlight-wrap-overlay nil)
+ '(sp-highlight-wrap-tag-overlay nil)
+ '(vc-annotate-background nil))
