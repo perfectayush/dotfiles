26f49f0481cf7f7a8f62e03d7a946e44bdc370a1
Ayush Goyal <ayush@archnemesis.sensehq.co>
2020-09-01 00:47:38 +0530

    Support both super/hyper with left/right command key on mac

--- emacs/init.el
+++ emacs/init.el
@@ -112,7 +112,8 @@ This function should only modify configuration layer settings."
      nginx
      slack
      spotify
-     osx
+     (osx :variables osx-command-as       'super
+                     osx-right-command-as 'hyper)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
