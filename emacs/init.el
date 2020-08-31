eeb92788877fc17f5d0642e94232321a0171d31c
Ayush Goyal <ayush@archnemesis.sensehq.co>
2020-09-01 00:42:46 +0530

    Fixes for new mac
    Emacs:
    * Not using fasd anymore so don't warn for fasd util
    * org-roam is not a layer anymore ???
    
    mpv:
    * Switch video output to new recommended value for mac
    * Disable glsl shared as the file is not present anymore

--- emacs/init.el
+++ emacs/init.el
@@ -101,7 +101,6 @@ This function should only modify configuration layer settings."
      ibuffer
      pandoc
      colors
-     fasd
      speed-reading
      command-log
      restclient
@@ -114,7 +113,6 @@ This function should only modify configuration layer settings."
      slack
      spotify
      osx
-     org-roam
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
