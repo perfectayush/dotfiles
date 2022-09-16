6682dc82e19300f9c5416639a887dfcb0e5c1404
Ayush Goyal <perfectayush@gmail.com>
2022-09-16 22:24:54 +0530

    Hammerspoon updates

--- hammerspoon/brightness.lua
+++ hammerspoon/brightness.lua
@@ -1,5 +1,7 @@
 -- manage screen brightness
 
+hs.redshift.start(3000, '20:00', '7:00', '2h')
+
 function brightness_toggle()
   local current_brightness = hs.brightness.get()
   if current_brightness > 0 then
@@ -7,4 +9,13 @@ function brightness_toggle()
   else
     hs.brightness.set(100)
   end
+end
+
+
+function greyscale_toggle()
+  hs.screen.setForceToGray(not hs.screen.getForceToGray())
+end
+
+function night_toggle()
+  hs.redshift.toggle()
 end
--- hammerspoon/emacs.lua
+++ hammerspoon/emacs.lua
@@ -1,11 +1,15 @@
--- open/hide emacs on shift + f12
-hs.hotkey.bind("shift", "F12", function()
-                 local emacs = hs.application.get('Emacs')
-                 if emacs == nil then
-                   hs.application.launchOrFocus('Emacs')
-                 elseif emacs:isFrontmost() then
-                   emacs:hide()
-                 else
-                   emacs:activate()
-                 end
-end)
+-- open/hide app on shift + f12
+
+function bring_forward_application(application)
+  local app = hs.application.get(application)
+  if app == nil then
+    hs.application.launchOrFocus(application)
+  elseif app:isFrontmost() then
+    app:hide()
+  else
+    app:activate()
+  end
+end
+
+hs.hotkey.bind("shift", "F12", function() bring_forward_application("Emacs") end)
+
